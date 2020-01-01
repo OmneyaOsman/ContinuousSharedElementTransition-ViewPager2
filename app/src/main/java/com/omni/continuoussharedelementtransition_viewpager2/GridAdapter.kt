a3fd5394ea2ea5e8c252d515145e2fadc9ce6dfd
Omni <engomneya.osman@gmail.com>
2020-01-01 23:35:30 +0200

    start

---
+++ app/src/main/java/com/omni/continuoussharedelementtransition_viewpager2/GridAdapter.kt
@@ -0,0 +1,2 @@
+package com.omni.continuoussharedelementtransition_viewpager2
+
---
+++ app/src/main/java/com/omni/continuoussharedelementtransition_viewpager2/ImageData.kt
@@ -0,0 +1,15 @@
+package com.omni.continuoussharedelementtransition_viewpager2
+
+import androidx.annotation.DrawableRes
+
+enum class ImageData ( @DrawableRes val imageResource: Int)
+{
+    ONE(R.drawable.pic_one),
+    TWO(R.drawable.pic_two),
+    THREE(R.drawable.pic_three),
+    FOUR(R.drawable.pic_four),
+    FIVE(R.drawable.pic_five),
+    SIX(R.drawable.pic_six),
+    SEVEN(R.drawable.pic_seven),
+    EIGHT(R.drawable.pic_eight)
+}
