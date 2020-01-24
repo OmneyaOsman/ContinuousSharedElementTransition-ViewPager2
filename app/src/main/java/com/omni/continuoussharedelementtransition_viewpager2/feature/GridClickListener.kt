5734c869c54c127826a341ea453ea89c80af2524
Omni <engomneya.osman@gmail.com>
2020-01-25 01:48:56 +0200

    add SharedElement Exit and Enter callBacks to make gridFragment aware of current position of Pager

--- app/src/main/java/com/omni/continuoussharedelementtransition_viewpager2/feature/GridClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.omni.continuoussharedelementtransition_viewpager2.feature
-
-import android.widget.ImageView
-
-interface GridClickListener {
-     val onClick: (Int, ImageView) -> Unit ?
-}
